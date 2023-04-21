66f1044fdf18c8779784a78b392ec3bef28b2c58
sabbellasri <34259114+sabbellasri@users.noreply.github.com>
2023-04-20 23:14:25 -0600

    version 1

--- gitshield/cli.py
+++ gitshield/cli.py
@@ -1,5 +1,6 @@
 import click
 import re
+import openai
 from utils import check_for_git, install_git_hook, uninstall_git_hook
 
 
@@ -36,11 +37,29 @@ def pre_commit(changed_file_path):
     """Run on the pre-commit hook"""
     file1 = open(changed_file_path[0], "r")
     fileString = file1.read()
-
+    print("hello")
     regex = r"\b\w{2}-\w{2}\b"
     # print(re.findall(regex, fileString))
-    if(len(re.findall(regex, fileString))):
-        print('There are secrets present in ', changed_file_path[0], ' secrets - ', re.findall(regex, fileString))
+    if len(re.findall(regex, fileString)):
+        print(
+            "There are secrets present in ",
+            changed_file_path[0],
+            " secrets - ",
+            re.findall(regex, fileString),
+        )
+    print(fileString)
+    openai.api_key = "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"
+    message = "are there any secrets in this code?" + fileString
+    messages = []
+
+    messages.append(
+        {"role": "user", "content": message},
+    )
+    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+
 
 if __name__ == "__main__":
     cli()
